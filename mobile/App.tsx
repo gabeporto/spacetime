96ae8dfb5f4720eff5ff9677e3635d6bd8f5b28a
Gabriel Porto <7gabrielporto@gmail.com>
2023-05-23 17:51:47 -0300

    feat: finish basic mobile home page

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native';
+
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+import { styled } from 'nativewind';
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  })
+
+  if(!hasLoadedFonts) {
+    return null
+  }
+
+
   return (
-    <View className='bg-zinc-950 flex-1 items-center justify-center'>
-      <Text className='text-zinc-50 font-bold text-5xl'>Spacetime</Text>
+    <ImageBackground  
+      source={blurBg} 
+      className="relative px-8 py-10 bg-zinc-950 flex-1 justify-center"
+      imageStyle={{ position: 'absolute', left: '-100%'}}
+    >
+
+      <StyledStripes className='absolute left-2' />
+
+
+      <View className='flex-1 items-center justify-center gap-6'>
+
+        <NLWLogo />
+
+        <View className='space-y-2'>
+          <Text className='text-center font-title text-2xl leading-tight text-gray-50'>
+            Your time capsule
+          </Text>
+
+          <Text className='text-center font-body text-base leading-tight text-gray-100'>
+            Collect memorable moments from your journey and share (if you like) with the world!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className='rounded-full bg-green-500 px-5 py-3'
+        >
+          <Text className='font-alt text-sm uppercase text-black'>
+            Register memory
+          </Text>
+        </TouchableOpacity>
+
+      </View>
+
+
+      <Text className='text-center font-body text-sm leading-relaxed text-gray-200'>Made with 💜 on NLW of Rocketseat</Text>
+
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   );
 }
